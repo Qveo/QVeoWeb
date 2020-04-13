638c26583cde223c649f322a2150d575febf6084
yuriy <yuriy.mraka@gmail.com>
2020-04-13 17:57:59 +0200

    Agregando fotos:  - peliculas  - series  - directores  - actores  - plataforma Actualización de la BBDD:  - En la carpeta de sql agrego los scripts

---
+++ sql/fotos.sql
@@ -0,0 +1,210 @@
+
+/* Fotos peliculas */
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/madagascar.png' WHERE (`ID` = '1');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/como_entrenar_a_tu_dragon.png' WHERE (`ID` = '2');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/madagascar.png' WHERE (`ID` = '3');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/project_x.png' WHERE (`ID` = '4');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/deep_blue_sea.png' WHERE (`ID` = '5');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/godzilla_2014.jpg' WHERE (`ID` = '6');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/ready_player_one.jpg' WHERE (`ID` = '7');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/noche_de_juegos.jpg' WHERE (`ID` = '8');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/el_francotirador.jpg' WHERE (`ID` = '9');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/origen.jpg' WHERE (`ID` = '10');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/spider_man.jpg' WHERE (`ID` = '11');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/la_purga_election_2016.jpg' WHERE (`ID` = '12');
+UPDATE `qveo`.`pelicula` SET `POSTER` = '/resources/img/peliculas/el_hobbit_la_desolacion_de_smaug.jpg' WHERE (`ID` = '13');
+
+/* Fotos series */
+ALTER TABLE qveo.serie ADD COLUMN POSTER VARCHAR(100) AFTER TITULO;
+
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/knightfall.jpg' WHERE (`ID` = '1');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/rick_and_morty.jpg' WHERE (`ID` = '2');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/vikings.jpg' WHERE (`ID` = '3');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/the_blacklist.jpg' WHERE (`ID` = '4');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/peaky_blinders.jpg' WHERE (`ID` = '5');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/community.jpg' WHERE (`ID` = '6');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/friends.jpg' WHERE (`ID` = '7');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/the_big_bang_theory.jpg' WHERE (`ID` = '8');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/el_ministerio_del_tiempo.jpg' WHERE (`ID` = '9');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/ozark.jpg' WHERE (`ID` = '10');
+UPDATE `qveo`.`serie` SET `POSTER` = '/resources/img/series/code_black.jpg' WHERE (`ID` = '11');
+
+/* Logo plataforma */
+ALTER TABLE qveo.plataforma ADD COLUMN LOGO VARCHAR(100) AFTER NOMBRE;
+
+UPDATE `qveo`.`plataforma` SET `LOGO` = '/resources/img/plataforma/hbo.jpg' WHERE (`ID` = '1');
+UPDATE `qveo`.`plataforma` SET `LOGO` = '/resources/img/plataforma/netflix.jpg' WHERE (`ID` = '2');
+UPDATE `qveo`.`plataforma` SET `LOGO` = '/resources/img/plataforma/amazon.jpg' WHERE (`ID` = '3');
+INSERT INTO `qveo`.`plataforma` (`NOMBRE`, `LOGO`) VALUES ('Disney Plus', '/resources/img/plataforma/disney.jpg');
+INSERT INTO `qveo`.`plataforma` (`NOMBRE`, `LOGO`) VALUES ('Filmin', '/resources/img/plataforma/filmin.jpg');
+INSERT INTO `qveo`.`plataforma` (`NOMBRE`, `LOGO`) VALUES ('Movistar', '/resources/img/plataforma/movistar.jpg');
+
+/* Fotos director */
+ALTER TABLE qveo.director ADD COLUMN FOTO VARCHAR(100) AFTER NOMBRE;
+
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/eric_darnell.jpg' WHERE (`ID` = '1');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/tom_mcgrath.jpg' WHERE (`ID` = '2');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/dean_deblois.jpg' WHERE (`ID` = '3');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/chris_sanders.jpg' WHERE (`ID` = '4');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/marc_forster.jpg' WHERE (`ID` = '5');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/nima_nourizadeh.jpg' WHERE (`ID` = '6');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/renny_harlin.jpg' WHERE (`ID` = '7');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/gareth_edwards.jpg' WHERE (`ID` = '8');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/steven_spielberg.jpg' WHERE (`ID` = '9');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/john_francis_daley.jpg' WHERE (`ID` = '10');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/jonathan_goldstein.jpg' WHERE (`ID` = '11');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/clint_eastwood.jpg' WHERE (`ID` = '12');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/christopher_nolan.jpg' WHERE (`ID` = '13');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/sam_raimi.jpg' WHERE (`ID` = '14');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/james_demonaco.jpg' WHERE (`ID` = '15');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/peter_jackson.jpg' WHERE (`ID` = '16');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/david_petrarca.jpg' WHERE (`ID` = '17');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/jeff_myers.jpg' WHERE (`ID` = '18');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/michael_hirst.jpg' WHERE (`ID` = '19');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/joe_carnahan.jpg' WHERE (`ID` = '20');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/otto_bathurst.jpg' WHERE (`ID` = '21');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/dan_harmon.jpg' WHERE (`ID` = '22');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/david_crane.jpg' WHERE (`ID` = '23');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/marc_vigil.jpg' WHERE (`ID` = '24');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/bill_dubuque.jpg' WHERE (`ID` = '25');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/michael_seitzman.jpg' WHERE (`ID` = '26');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/' WHERE (`ID` = '27');
+UPDATE `qveo`.`director` SET `FOTO` = '/resources/img/directores/' WHERE (`ID` = '28');
+
+/* Foto actor */
+ALTER TABLE qveo.actor ADD COLUMN FOTO VARCHAR(100) AFTER NOMBRE;
+
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/ben_stiller.jpg' WHERE (`ID` = '1');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/chris_rock.jpg' WHERE (`ID` = '2');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/bernie_mac.jpg' WHERE (`ID` = '7');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/david_schwimmer.jpg' WHERE (`ID` = '3');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jada_pinkett.jpg' WHERE (`ID` = '4');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/cedric_antonio_kyles.jpg' WHERE (`ID` = '5');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/andy_richter.jpg' WHERE (`ID` = '6');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/alec_baldwin.jpg' WHERE (`ID` = '8');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/sherri_shepherd.jpg' WHERE (`ID` = '9');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/william_james_adams.jpg' WHERE (`ID` = '10');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/john_dimaggio.jpg' WHERE (`ID` = '11');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/conrad_vernon.jpg' WHERE (`ID` = '12');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/sacha_baron_cohen.jpg' WHERE (`ID` = '13');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jay_baruchel.jpg' WHERE (`ID` = '14');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/gerard_butler.jpg' WHERE (`ID` = '15');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/america_ferrera.jpg' WHERE (`ID` = '16');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/kristen_wiing.jpg' WHERE (`ID` = '17');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jonah_hill.jpg' WHERE (`ID` = '18');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/todd_joseph_miller.jpg' WHERE (`ID` = '19');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/christopher_mintz_plasse.jpg' WHERE (`ID` = '20');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/craig_ferguson.jpg' WHERE (`ID` = '21');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/william_bradley_pitt.jpg' WHERE (`ID` = '22');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/mireille_enos.jpg' WHERE (`ID` = '23');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/fana_mokoena.jpg' WHERE (`ID` = '24');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/daniella_kertesz.jpg' WHERE (`ID` = '25');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/james_badge_dale.jpg' WHERE (`ID` = '26');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/thomas_mann.jpg' WHERE (`ID` = '27');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/oliver_cooper.jpg' WHERE (`ID` = '28');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jonathan_daniel_brown.jpg' WHERE (`ID` = '29');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/thomas_jane.jpg' WHERE (`ID` = '30');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/saffron_dominique_burrows.jpg' WHERE (`ID` = '31');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jacqueline_susan_mckenzie.jpg' WHERE (`ID` = '32');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/stellan_john_skarsgard.jpg' WHERE (`ID` = '33');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/samuel_l_jackson.jpg' WHERE (`ID` = '34');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/james_todd_smith_III.jpg' WHERE (`ID` = '35');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/michael_david_rapaport.jpg' WHERE (`ID` = '36');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/aaron_johnson.jpg' WHERE (`ID` = '37');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/ken_watanabe.jpg' WHERE (`ID` = '38');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/elizabeth_olsen.jpg' WHERE (`ID` = '39');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/juliette_binoche.jpg' WHERE (`ID` = '40');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/sally_hawkins.jpg' WHERE (`ID` = '41');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/david_strathairn.jpg' WHERE (`ID` = '42');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/bryan_cranston.jpg' WHERE (`ID` = '43');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/tye_sheridan.jpg' WHERE (`ID` = '44');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/olivia_cooke.jpg' WHERE (`ID` = '45');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/ben_mendelsohn.jpg' WHERE (`ID` = '46');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/hannah_john_kamen.jpg' WHERE (`ID` = '47');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/simon_pegg.jpg' WHERE (`ID` = '48');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/mark_rylance.jpg' WHERE (`ID` = '49');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/rachel_anne_mcadams.jpg' WHERE (`ID` = '50');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/billy_magnussen.jpg' WHERE (`ID` = '51');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/kylie_bunbury.jpg' WHERE (`ID` = '52');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jesse_plemons.jpg' WHERE (`ID` = '53');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/michael_c_hall.jpg' WHERE (`ID` = '54');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/kyle_chandler.jpg' WHERE (`ID` = '55');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jason_bateman.jpg' WHERE (`ID` = '56');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/sharon_horgan.jpg' WHERE (`ID` = '57');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/lamorne_morris.jpg' WHERE (`ID` = '58');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/bradley_cooper.jpg' WHERE (`ID` = '59');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/sienna_miller.jpg' WHERE (`ID` = '60');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/leonardo_dicaprio.jpg' WHERE (`ID` = '61');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/ellen_page.jpg' WHERE (`ID` = '62');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/marion_cotillard.jpg' WHERE (`ID` = '63');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/joseph_gordon_levitt.jpg' WHERE (`ID` = '64');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/tom_hardy.jpg' WHERE (`ID` = '65');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/cillian_murphy.jpg' WHERE (`ID` = '66');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/michael_caine.jpg' WHERE (`ID` = '67');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/tobey_maguire.jpg' WHERE (`ID` = '68');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/willem_dafoe.jpg' WHERE (`ID` = '69');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/kirsten_dunst.jpg' WHERE (`ID` = '70');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/james_franco.jpg' WHERE (`ID` = '71');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/cliff_robertson.jpg' WHERE (`ID` = '72');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/rosemary_harris.jpg' WHERE (`ID` = '73');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jonathan_kimble_simmons.jpg' WHERE (`ID` = '74');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/frank_grillo.jpg' WHERE (`ID` = '75');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/elizabeth_mitchell.jpg' WHERE (`ID` = '76');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/mykelti_williamson.jpg' WHERE (`ID` = '77');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/ian_mckellen.jpg' WHERE (`ID` = '78');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/martin_freeman.jpg' WHERE (`ID` = '79');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/richard_armitage.jpg' WHERE (`ID` = '80');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/benedict_cumberbatch.jpg' WHERE (`ID` = '81');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/tom_cullen.jpg' WHERE (`ID` = '82');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jim_carter.jpg' WHERE (`ID` = '83');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/padraic_delaney.jpg' WHERE (`ID` = '84');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/simon_merrells.jpg' WHERE (`ID` = '85');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/justin_roiland.jpg' WHERE (`ID` = '86');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/spencer_grammer.jpg' WHERE (`ID` = '87');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/sarah_chalke.jpg' WHERE (`ID` = '88');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/chris_parnell.jpg' WHERE (`ID` = '89');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/travis_fimmel.jpg' WHERE (`ID` = '90');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/katheryn_winnick.jpg' WHERE (`ID` = '91');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/clive_standen.jpg' WHERE (`ID` = '92');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jessalyn_gilsig.jpg' WHERE (`ID` = '93');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/james_spader.jpg' WHERE (`ID` = '94');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/megan_boone.jpg' WHERE (`ID` = '95');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/diego_klattenhoff.jpg' WHERE (`ID` = '96');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/ryan_eggold.jpg' WHERE (`ID` = '97');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/helen_mccrory.jpg' WHERE (`ID` = '98');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/paul_anderson.jpg' WHERE (`ID` = '99');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/sophie_rundle.jpg' WHERE (`ID` = '100');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/joel_mchale.jpg' WHERE (`ID` = '101');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/john_oliver.jpg' WHERE (`ID` = '102');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/chevy_chase.jpg' WHERE (`ID` = '103');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jennifer_aniston.jpg' WHERE (`ID` = '104');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/courtney_cox.jpg' WHERE (`ID` = '105');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/lisa_kudrow.jpg' WHERE (`ID` = '106');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/matt_leblanc.jpg' WHERE (`ID` = '107');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/matthew_perry.jpg' WHERE (`ID` = '108');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/jim_parsons.jpg' WHERE (`ID` = '109');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/melissa_rauch.jpg' WHERE (`ID` = '110');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/kaley_cuoco.jpg' WHERE (`ID` = '111');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/johnny_galecki.jpg' WHERE (`ID` = '112');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/kunal_nayar.jpg' WHERE (`ID` = '113');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/simon_helberg.jpg' WHERE (`ID` = '114');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/mayim_bialik.jpg' WHERE (`ID` = '115');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/rodolfo_sancho.jpg' WHERE (`ID` = '116');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/aura_garrido.jpg' WHERE (`ID` = '117');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/nacho_fresneda.jpg' WHERE (`ID` = '118');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/cayetana_guillen_cuervo.jpg' WHERE (`ID` = '119');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/julia_garner.jpg' WHERE (`ID` = '120');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/laura_linney.jpg' WHERE (`ID` = '121');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/sofia_hublitz.jpg' WHERE (`ID` = '122');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/melanie_chandra.jpg' WHERE (`ID` = '123');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/bonnie_somerville.jpg' WHERE (`ID` = '124');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/raza_jaffrey.jpg' WHERE (`ID` = '125');
+UPDATE `qveo`.`actor` SET `FOTO` = '/resources/img/actores/marcia_gay_harden.jpg' WHERE (`ID` = '126');
+
+
+
+
+/* INSERTAMOS LOS ROLES */
+INSERT INTO qveo.rol (NOMBRE) VALUES('user');
+INSERT INTO qveo.rol (NOMBRE) VALUES('admin');
+
