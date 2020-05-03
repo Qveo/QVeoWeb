3ac6fddd54389bd096755e5462d2ff5de2cc987d
AlexxDan <alejandronavaroli@gmail.com>
2020-05-03 11:20:43 +0200

    validacion

--- src/main/java/com/qveo/qveoweb/model/Serie.java
+++ src/main/java/com/qveo/qveoweb/model/Serie.java
@@ -3,7 +3,6 @@ package com.qveo.qveoweb.model;
 import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
