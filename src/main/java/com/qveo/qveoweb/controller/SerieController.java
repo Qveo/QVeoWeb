f13f8c07aacbd1e06d99a42ca8efcd81989e9ece
AlexxDan <alejandronavaroli@gmail.com>
2020-05-25 18:35:25 +0200

    Error chips NO resueltos

--- src/main/java/com/qveo/qveoweb/controller/SerieController.java
+++ src/main/java/com/qveo/qveoweb/controller/SerieController.java
@@ -147,17 +147,7 @@ public class SerieController {
 
 				return "series/registroSerie";
 			}
-			System.out.println("----Director");
-			for (Director direct : serieNueva.getDirectores()) {
-				System.out.println(direct.getNombre()+" "+direct.getId());
-			}
 			
-			System.out.println("----Actor");
-			
-			for (Actor actor : serieNueva.getActores()) {
-				System.out.println(actor.getNombre()+" "+actor.getId());
-			}
-
 			serieService.save(serieNueva, file);
 			status.setComplete();
 		} catch (IOException e) {
--- src/main/java/com/qveo/qveoweb/validation/SerieValidador.java
+++ src/main/java/com/qveo/qveoweb/validation/SerieValidador.java
@@ -1,8 +1,5 @@
 package com.qveo.qveoweb.validation;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -22,27 +19,12 @@ public class SerieValidador implements Validator {
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
-
+	
 		Serie serie = (Serie) target;
 
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "titulo", "empty");
-		System.out.println("Hola");
+
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "fechaInicio", "empty");
-
-	
-
-//		if (serie.getFechaInicio() != null) {
-//			DateFormat format2 = new SimpleDateFormat("dd-MM-yyyy");
-//			String fecha = format2.format(serie.getFechaInicio());
-//			System.out.println(fecha);
-//
-//			if (!fecha.matches("[\\d]{2}[-][\\d]{2}[-][\\d]{4}")) {
-//				throw new IllegalArgumentException("El formato no es correcto, introduce la f);
-//
-//			}
-//		}
-		
 
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "sinopsis", "empty");
 
